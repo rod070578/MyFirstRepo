e33669d870ee045007726cf1c714d451dd29ae81
rod070578 <rodrigomasangya@gmail.com>
2015-08-06 08:20:46 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: rodrigo
+ *
+ * Created on August 5, 2015, 11:37 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+   int howmany;
+    cout << "Hello" << endl;
+    cin >> howmany;
+    while(howmany-- > 0)
+         cout << "Hello" << endl;
+  
+    return 0;
+}
+
